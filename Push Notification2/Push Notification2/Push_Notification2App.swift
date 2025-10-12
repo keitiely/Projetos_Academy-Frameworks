c6412f1440f3512382c52a8381e735f048c92c68
Keitiely Silva <keitielysilvaviana@gmail.com>
2025-10-12 09:07:42 -0300

    Projeto Push Notification com CloudKit com configuracao do CloudKit padrao

---
+++ Push Notification2/Push Notification2/Push_Notification2App.swift
@@ -0,0 +1,17 @@
+//
+//  Push_Notification2App.swift
+//  Push Notification2
+//
+//  Created by Keitiely Silva Viana on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Push_Notification2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
