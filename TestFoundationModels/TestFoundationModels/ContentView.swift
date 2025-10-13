8eb5ddbeeaef8005e96f207056e6a14ead713568
Keitiely Silva <keitielysilvaviana@gmail.com>
2025-10-13 11:53:55 -0300

    teste 1 do video: WWDC25: Code-along: Bring on-device AI to your app using the Foundation Models framework

---
+++ TestFoundationModels/TestFoundationModels/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestFoundationModels
+//
+//  Created by Keitiely Silva Viana on 13/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestFoundationModels/TestFoundationModels/TestFoundationModelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestFoundationModelsApp.swift
+//  TestFoundationModels
+//
+//  Created by Keitiely Silva Viana on 13/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TestFoundationModelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
