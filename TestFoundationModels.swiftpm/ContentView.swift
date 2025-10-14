bf881ce410155e02c747fdfae20e7acf2f95832b
Keitiely Silva <keitielysilvaviana@gmail.com>
2025-10-14 10:44:53 -0300

    Atualizando o testFoundationModel com instrução ao modelo

--- TestFoundationModels.swiftpm/ContentView.swift
+++ TestFoundationModels.swiftpm/ContentView.swift
@@ -12,9 +12,18 @@ struct ContentView: View {
             {
                 TextField("Question", text: $question)
                 
-                Button("Ask Question"){
-                    if #available(iOS 26.0, *) {
-                        let session = LanguageModelSession()
+                Button("Ask Question"){ if #available(iOS 26.0, *) {
+                let instructions = Instructions{
+                    """
+                    Voce é um analisador de musicas que ira listar somente as 5 muiscas principais do artista
+                    sua resposta precisa ser, escreva literalmete assim:
+                
+                    Top 1 <musica tal>
+                    Top 2 <musica tal>
+                    outros top ate 5
+                
+            """ }
+              let session = LanguageModelSession(instructions: instructions)
                         Task {
                             do {
                                 let response = try await session.respond(to: question)
