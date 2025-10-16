d3ac84766aeada8f1f56aaff884b8c2278951d05
Keitiely Silva <keitielysilvaviana@gmail.com>
2025-10-16 04:12:04 -0300

    Projeto CRUD concluido

---
+++ CloudKitPushNotifcation/CloudKitPushNotifcation/CloudKitPushNotifcationApp.swift
@@ -0,0 +1,18 @@
+//
+//  CloudKitPushNotifcationApp.swift
+//  CloudKitPushNotifcation
+//
+//  Created by Keitiely Silva Viana on 15/10/25.
+//
+
+import SwiftUI
+
+@main
+struct CloudKitPushNotifcationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CloudKitCrudBootcamp()
+//            ContentView()
+        }
+    }
+}
