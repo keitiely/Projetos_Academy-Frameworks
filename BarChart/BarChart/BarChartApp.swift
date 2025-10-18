a0916f920305e13ba0bca33143b63b7915ebfa1e
Keitiely Silva <keitielysilvaviana@gmail.com>
2025-10-18 14:52:45 -0300

    Projetinho de Graficos

---
+++ BarChart/BarChart/BarChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarChartApp.swift
+//  BarChart
+//
+//  Created by Keitiely Silva Viana on 17/10/25.
+//
+
+import SwiftUI
+
+@main
+struct BarChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
