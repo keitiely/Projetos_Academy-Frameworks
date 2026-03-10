7522f5bae618cdbc957dce63442898cffba29fe2
Keitiely Silva <keitielysilvaviana@gmail.com>
2026-03-10 15:55:31 -0300

    feat: update teste

---
+++ BarChart/testebarchart2/testebarchart2.swift
@@ -0,0 +1,35 @@
+//
+//  testebarchart2.swift
+//  testebarchart2
+//
+//  Created by Keitiely Silva Viana on 10/03/26.
+//
+
+import Testing
+@testable import BarChart
+
+struct testebarchart2 {
+    
+    
+    @Suite("testando dados")
+    struct testandoDados {
+        @Test func test_verificandodados()  {
+            
+           //arrange
+            var dados : Int
+            let content = ContentView()
+
+            //act
+            
+            dados = contentView.
+            
+        }
+
+        
+    }
+    
+    
+    
+    
+}
+
